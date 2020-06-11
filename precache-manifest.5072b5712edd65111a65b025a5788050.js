eb254ff477d9b19c057890e9b50f43941a070dd8
gode429 <gode429@gmail.com>
2020-06-12 01:41:49 +0530

    Initial commit

---
+++ precache-manifest.5072b5712edd65111a65b025a5788050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7f0b267ee475069c0c3441d309c4ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f8be6c90e0b7c3f5ac7",
+    "url": "/static/css/main.9e767f70.chunk.css"
+  },
+  {
+    "revision": "7cbec0c10d7b31aed645",
+    "url": "/static/js/2.696c8bdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.696c8bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8be6c90e0b7c3f5ac7",
+    "url": "/static/js/main.8de83a30.chunk.js"
+  },
+  {
+    "revision": "672cefe876e9dd8c7cfb",
+    "url": "/static/js/runtime-main.cdf52f05.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL : 'https://react-burger-builder-87001.firebaseio.com/'
-})
-
-export default instance;
